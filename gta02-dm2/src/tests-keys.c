99ce11cbed11af0e250dee84c3d0e574878dd166
Andy Green <andy@openmoko.com>
2008-05-11 12:37:23 +0100

    fix-key-test.patch
    The keys test never worked properly it seems.
    Changed the code so it reports up and down activity
    for duration of each test
    
    Signed-off-by: Andy Green <andy@openmoko.com>

--- gta02-dm2/src/tests-keys.c
+++ gta02-dm2/src/tests-keys.c
@@ -4,17 +4,13 @@
 
 static void key_press(char *key )
 {
-	int fd, rd, i, j, k;
-	struct input_event ev[64];
-	int version;
-	unsigned short id[4];
-	unsigned long bit[EV_MAX][NBITS (KEY_MAX)];
-	char name[256] = "Unknown";
-	int abs[5];
+	int fd, rd;
+	struct input_event ev;
 	char buf[BUFSIZ];
-	int count = 2;
+	int count = 8;
 	fd_set fds;
 	struct timeval tv;
+	const char *description;
 
 	/* Confirm device initialized */
 	if (access(key, R_OK)) {
@@ -29,48 +25,8 @@ static void key_press(char *key )
 		exit (1);
 	}
 
-	ioctl(fd, EVIOCGVERSION, &version);
-	printf("Input driver version is %d.%d.%d\n",
-			version >> 16, (version >> 8) & 0xff, version & 0xff);
-
-	ioctl(fd, EVIOCGID, id);
-	printf("Input device ID: bus 0x%x vendor 0x%x "
-	       "product 0x%x version 0x%x\n",
-	       id[ID_BUS], id[ID_VENDOR], id[ID_PRODUCT], id[ID_VERSION]);
-
-	ioctl(fd, EVIOCGNAME (sizeof (name)), name);
-	printf("Input device name: \"%s\"\n", name);
-
-	memset(bit, 0, sizeof (bit));
-	ioctl(fd, EVIOCGBIT (0, EV_MAX), bit[0]);
-	printf("Supported events:\n");
-
-	for (i = 0; i < EV_MAX; i++) {
-		if (!test_bit(i, bit[0]))
-			continue;
-
-		printf("  Event type %d (%s)\n", i,
-						 events[i] ? events[i] : "?");
-		ioctl(fd, EVIOCGBIT (i, KEY_MAX), bit[i]);
-		for (j = 0; j < KEY_MAX; j++) {
-			if (!test_bit(j, bit[i]))
-				continue;
-			printf("    Event code %d (%s)\n", j, names[i] ?
-				(names[i][j] ? names[i][j] : "?") : "?");
-			if (i != EV_ABS)
-				continue;
-			ioctl(fd, EVIOCGABS (j), abs);
-			for (k = 0; k < 5; k++)
-				if ((k < 3) || abs[k])
-					printf("      %s %6d\n",
-							     absval[k], abs[k]);
-		}
-	}
-
 	oltk_view_set_text(view, "Waiting For Key Press\n");
 	oltk_redraw(oltk);
-
-	//	read (fd, ev, sizeof (ev));
 
 	rd = 1;
 	do {
@@ -79,23 +35,15 @@ static void key_press(char *key )
 		tv.tv_sec = 0;
 		tv.tv_usec = 200000;
 		if (select(fd + 1, &fds, NULL, NULL, &tv) > 0)
-			read (fd, ev, sizeof (struct input_event));
+			read (fd, &ev, sizeof (struct input_event));
 		else
 			rd = 0;
 	} while (rd);
 
-
-	close(fd);
-
-	if ((fd = open (key, O_RDONLY)) < 0) {
-		perror ("evtest");
-		exit (1);
-	}
-
 	printf("Waiting For Key Press\n");
 
 	while (count) {
-		rd = read (fd, ev, sizeof(ev));
+		rd = read (fd, &ev, sizeof(ev));
 		printf("read %d / %d\n", rd, sizeof(struct input_event));
 		if (rd < (int) sizeof(struct input_event)) {
 			printf("yyy\n");
@@ -103,20 +51,36 @@ static void key_press(char *key )
 			exit(1);
 		}
 
-		for (i = 0; i < rd / sizeof(struct input_event); i++) {
-			snprintf(buf, BUFSIZ,
-				 "Key Press Down!! \n(type %d (%s), "
-				 "code %d (%s), value %d)\n", ev[i].type,
-				 events[ev[i].type] ? events[ev[i].type] : "?",
-				 ev[i].code, names[ev[i].type] ?
-				  (names[ev[i].type][ev[i].code] ?
-				   names[ev[i].type][ev[i].code] : "?") : "?",
-				 ev[i].value);
-			printf("%s", buf);
+		if (!ev.type)
+			continue;
 
-			oltk_view_set_text(view, buf);
-			oltk_redraw(oltk);
+		switch (ev.value) {
+		case 0:
+			description = "UP";
+			break;
+		case 1:
+			description = "DOWN";
+			break;
+		case 2:
+			description = "?";
+			break;
 		}
+		
+		snprintf(buf, BUFSIZ,
+				"Key Press %s!! \n(type %d (%s), "
+				"code %d (%s), value %d)\n", description, ev.type,
+				events[ev.type] ? events[ev.type] : "?",
+				ev.code, names[ev.type] ?
+				(names[ev.type][ev.code] ?
+				names[ev.type][ev.code] : "?") : "?",
+				ev.value);
+
+
+		printf("%s", buf);
+
+		oltk_view_set_text(view, buf);
+		oltk_redraw(oltk);
+
 		count--;
 	}
 }
